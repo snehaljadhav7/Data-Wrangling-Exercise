08166f712323a1af5e701254abe6b4b5c11a997e
snehaljadhav7 <snehal10.jadhav@gmail.com>
2021-02-25 16:33:14 -0500

    Scraped table from HTML page

--- python_script.py
+++ python_script.py
@@ -1,2 +1,14 @@
 import pandas as pd 
 from bs4 import BeautifulSoup
+from urllib.request import urlopen 
+path = 'https://www.eia.gov/dnav/ng/hist/rngwhhdD.htm'
+price_list = []
+header_list = []
+page = urlopen(path)
+soup = BeautifulSoup(page,'html.parser') 
+table = soup.find(text="Week Of").find_parent("table")
+
+print(table)
+
+
+
